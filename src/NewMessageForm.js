193a00a2917ad8434073bb0abb29fc1a7fc6a18b
btkkzc <btkkzc@gmail.com>
2022-09-15 20:15:10 +0200

    test added

---
+++ src/NewMessageForm.js
@@ -0,0 +1,21 @@
+import React, {useState} from 'react'
+
+function NewMessageForm({onSend}) {
+
+    const [inputText, setInputText] = useState('');
+
+    function handleTextChange(event) { setInputText(event.target.value);}
+    
+    function handleSend() {
+        onSend(inputText);
+        setInputText('');}
+
+  return (
+    <>
+        <input type="text" data-testid="messageText" value={inputText} onChange={handleTextChange} />
+        <button data-testid="sendButton" onClick={handleSend} >  Send    </button>
+    </>
+  )
+}
+
+export default NewMessageForm
